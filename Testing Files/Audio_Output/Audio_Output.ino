0862a8974b83272d1cd99b37fcf781bdf552f0e4
Karthik Raja <kar304@pitt.edu>
2025-03-11 22:52:45 -0400

    Audio Works Perfectly Fine

--- Testing Files/Audio_Output/Audio_Output.ino
+++ Testing Files/Audio_Output/Audio_Output.ino
@@ -1,41 +1,23 @@
-#include <AltSoftSerial.h>
-#include <DFRobotDFPlayerMini.h>
+#include "mp3tf16p.h"
 
-const int RXPin = 8;
-const int TXPin = 9;
+MP3Player mp3(8, 9);
 
-AltSoftSerial mySerial;  // Uses fixed pins (TX=9, RX=8)
-DFRobotDFPlayerMini player;
+int randomInt = 0;
+bool playedOnce = false;
 
 void setup() {
-    Serial.begin(9600);         // Start Serial Monitor
-    mySerial.begin(9600);       // Start communication with DFPlayer Mini
-
-    Serial.println("Initializing DFPlayer Mini...");
-
-    if (!player.begin(mySerial)) {
-        Serial.println("DFPlayer Mini initialization failed!");
-        while (true); // Stop execution if DFPlayer doesn't initialize
-    }
-
-    Serial.println("DFPlayer Mini initialized successfully.");
-    Serial.println("Sending play command...");
-
-    player.volume(30);  // Set volume
-    player.play(1);     // Play track 001 in folder 01
-
-    Serial.println("If the DFPlayer is working, it should now be playing track 001.");
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mp3.initialize();
 }
 
 void loop() {
-    // Read the DFPlayer's status
-    if (player.available()) {
-        int status = player.readType();
-        int value = player.read();
 
-        Serial.print("DFPlayer Status: ");
-        Serial.print(status);
-        Serial.print(" | Value: ");
-        Serial.println(value);
-    }
+  if (!playedOnce) {
+    randomInt = random(1, 3);
+    mp3.playTrackNumber(randomInt, 10);
+    mp3.serialPrintStatus(MP3_ALL_MESSAGE);
+    playedOnce = true;
+  }
+
 }
